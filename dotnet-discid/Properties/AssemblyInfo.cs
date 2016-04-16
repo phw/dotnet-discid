e93586126908a8318d18997148bf89794545ae78
Tim Van Holder <tim.vanholder@gmail.com>
2016-04-16 17:55:54 +0200

    Bumped version to 0.2.1. Tweaked copyright years. Updated NuGet release notes.

--- dotnet-discid/Properties/AssemblyInfo.cs
+++ dotnet-discid/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Philipp Wolfer")]
 [assembly: AssemblyProduct("dotnet-discid")]
-[assembly: AssemblyCopyright("Copyright © Philipp Wolfer 2013")]
+[assembly: AssemblyCopyright("Copyright © Philipp Wolfer 2013, 2015, 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.2.1.0")]
+[assembly: AssemblyFileVersion("0.2.1.0")]
